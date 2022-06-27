9a5785459ce79ff124f3c2060185974801b2c731
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-06-28 02:48:34 +0900

    grade shell script

---
+++ grademe.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+flag=0
+if [ ! -e ./a.out ]; then
+	clang++ $1.cpp
+	flag=1
+fi
+
+for i in 1 2 3 4 5
+do
+	path=../CPS\(채점폴더\)/`ls ../CPS\(채점폴더\) | grep "^$1[.+]"`
+	./a.out < "${path}/in${i}.txt" > out
+	value=`diff out "${path}/out${i}.txt"`
+	rm out
+	echo -n "Test ${i}: "
+	if [ -z "${value}" ]; then
+		echo -e "\033[32;40mok\033[0m"
+	else
+		echo -e "\033[31;40mfail\033[0m"
+	fi
+done
+
+if [ ${flag} -eq 1 ]; then
+	rm ./a.out
+fi
