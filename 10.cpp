d2e80efd7bc433812de3ef2e6b6d6f6477969998
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-06-30 21:07:15 +0900

    10. 자릿수의 합

--- 10.cpp
+++ 10.cpp
@@ -3,23 +3,31 @@
 
 using namespace std;
 
+int digit_sum(int x)
+{
+	int sum = 0;
+	while (x > 0)
+	{
+		sum += x % 10;
+		x /= 10;
+	}
+	return sum;
+}
+
 int main()
 {
 	int n;
 	cin >> n;
 
-	string num;
-	int sum = 0, tmp, max = 0;
+	int num, sum = 0, tmp, max = 0;
 	for (int i = 0; i < n; i++)
 	{
 		cin >> num;
-		tmp = 0;
-		for (auto c : num)
-			tmp += c - 48;
+		tmp = digit_sum(num);
 		if (tmp >= sum)
 		{
-			if (!(tmp == sum && stoi(num) < max))
-				max = stoi(num);
+			if (!(tmp == sum && num < max))
+				max = num;
 			sum = tmp;
 		}
 	}
