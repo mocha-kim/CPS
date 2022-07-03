508cc72222e2633b9e592ae3b065fb0e9fab08ba
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-07-03 16:19:38 +0900

    29-30. 3의 개수는?

---
+++ basic/30.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int help, count = 0;
+
+	string num_str = to_string(n);
+	int cur, left, right;
+	int k = 1;
+	for (int i = num_str.size() - 1; i >= 0; i--)
+	{
+		cur = num_str[i] - 48;
+		left = n / (k * 10);
+		right = n % k;
+		if (cur > 3)
+			count += (left + 1) * k;
+		else if (cur == 3)
+			count += left * k + right + 1;
+		else
+			count += left * k;
+		k *= 10;
+	}
+	cout << count;
+}
