eeaf9ec716686132343624e1c66dd8f7df1f663f
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-07-22 11:44:03 +0900

    38. Inversion Sequence

--- sort/38.cpp
+++ sort/38.cpp
@@ -9,27 +9,39 @@ int main()
 	int n;
 	cin >> n;
 
-	int num, count, pos;
-	vector<int> sorted(n);
-	cin >> num;
-	sorted[num] = 1;
-	for (int i = 1; i < n; i++)
+	// int num, count, pos;
+	// vector<int> original(n);
+	// cin >> num;
+	// original[num] = 1;
+	// for (int i = 1; i < n; i++)
+	// {
+	// 	cin >> num;
+	// 	count = 0;
+	// 	pos = 0;
+	// 	for (int j = 0; j < n; j++)
+	// 	{
+	// 		if (count == num && original[pos] == 0)
+	// 			break;
+	// 		if (original[j] == 0)
+	// 			count++;
+	// 		pos++;
+	// 	}
+	// 	original[pos] = i + 1;
+	// }
+
+	vector<int> inversion(n);
+	for (int i = 0; i < n; i++)
+		cin >> inversion[i];
+
+	vector<int> original;
+	for (int i = n - 1; i >= 0; i--)
 	{
-		cin >> num;
-		count = 0;
-		pos = 0;
-		for (int j = 0; j < n; j++)
-		{
-			if (count == num && sorted[pos] == 0)
-				break;
-			if (sorted[j] == 0)
-				count++;
-			pos++;
-		}
-		sorted[pos] = i + 1;
+		auto pos = original.begin() + inversion[i];
+		original.insert(pos, i + 1);
 	}
+
 	string answer;
-	for (int i : sorted)
+	for (int i : original)
 		answer.append(to_string(i) + " ");
 	answer.pop_back();
 	cout << answer;
